df0a96f83af22878e688a60629ba20bebef0156a
Prabin Basnet <prabinbasnet09@gmail.com>
2022-12-12 17:13:05 -0600

    installed and configured autopep8

---
+++ backend/notesApp/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from django.template import Context, loader
+from django.http import HttpResponse, JsonResponse
+
+
+def DefaultView(request):
+    return HttpResponse("This Django project contains the Notes App")
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    # setting safe allows us to send more than just a Dictionary
+    return JsonResponse(routes, safe=False)
