7f3a915a347e845ae45fa0568f063e83df8ca4ba
Prabin Basnet <prabinbasnet09@gmail.com>
2022-12-16 01:21:14 -0600

    Created a RESTFUL API

---
+++ backend/notesApp/utils.py
@@ -0,0 +1,42 @@
+from rest_framework.response import Response
+from .serializers import NotesSerializer
+from .models import Notes
+
+
+def getAllNotes(request):
+    notes = Notes.objects.all()
+    serializer = NotesSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+def createNote(request):
+    data = request.data
+    note = Notes.objects.create(
+        title=data['title'],
+        body=data['body'],
+    )
+    serializer = NotesSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+def getNoteInfo(request, id):
+    note = Notes.objects.get(id=id)
+    serializer = NotesSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+def deleteNote(request, id):
+    note = Notes.objects.get(id=id)
+    note.delete()
+    return Response('Note was deleted')
+
+
+def updateNote(request, id):
+    data = request.data
+    note = Notes.objects.get(id=id)
+    serializer = NotesSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
